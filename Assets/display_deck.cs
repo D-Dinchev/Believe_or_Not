f83797a51f4692bd053d97a9cff7a3ba2e2c27de
D-Dinchev <dincev.danil@gmail.com>
2022-05-22 16:37:31 +0300

    cart fit on the table added

---
+++ Assets/display_deck.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display_deck : MonoBehaviour
+{
+    [SerializeField] player_manager playerManager;
+    public Transform HandDeck;
+    private int howManyAdded = 0;
+    public int gapBetweenCards = 10;
+
+    public void FitCards()
+    {
+        if (playerManager.PlayersDeck.Count == 0) return;
+
+        foreach (var cardTexture in playerManager.PlayersDeck)
+        {
+            GameObject card = new GameObject();
+            Sprite cardSprite = Sprite.Create(cardTexture, new Rect(0f, 0f, cardTexture.width, cardTexture.height), new Vector2(0.5f, 0.5f), 100);
+            SpriteRenderer sr = card.AddComponent<SpriteRenderer>() as SpriteRenderer;
+            card.transform.position = HandDeck.transform.position + new Vector3(howManyAdded * gapBetweenCards, 0, 0);
+            card.transform.SetParent(HandDeck);
+            sr.sprite = cardSprite;
+
+            howManyAdded++;
+        }
+    }
+}
